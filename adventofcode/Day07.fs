77824b071a1d60b95d27ad93d5cf8f1e1786aa05
dawe <dawedawe@gmx.de>
2019-09-29 21:45:32 +0200

    Day 7 part 2

--- adventofcode/Day07.fs
+++ adventofcode/Day07.fs
@@ -18,6 +18,13 @@ module Day07 =
         {
             Name : char
             Prereqs : char list
+            TimeRequired : int
+        }
+
+    type WorkerState =
+        {
+            Id : int
+            WorksOn : char
         }
 
     let parseLine (s : string) =
@@ -26,14 +33,19 @@ module Day07 =
             Name = s.[36]
         }
 
+    let charToDuration (c : char) =
+        int(c) - 4
+
     let getSteps deps =
         let allSteps = Array.fold (fun s (d : Dependency) -> Set.union s (Set.ofList [d.Name; d.Prereq])) Set.empty deps
         let lookup = deps.ToLookup(fun d -> d.Name)
-        let stepsWithDeps = lookup.Select(fun g -> { Name =  g.Key;
-                                                     Prereqs = List.ofSeq(lookup.[g.Key].Select(fun v -> v.Prereq)) })
+        let stepsWithDeps = lookup.Select(fun g -> { Name =  g.Key
+                                                     Prereqs = List.ofSeq(lookup.[g.Key].Select(fun v -> v.Prereq))
+                                                     TimeRequired = charToDuration g.Key
+                                                    })
                                   .ToList()
         let startSteps = Set.difference allSteps (Set.ofSeq(stepsWithDeps.Select(fun s -> s.Name)))
-        let steps = Set.map (fun s -> { Name = s; Prereqs = List.empty }) startSteps 
+        let steps = Set.map (fun s -> { Name = s; Prereqs = List.empty; TimeRequired = charToDuration s }) startSteps 
                     |> Set.union (Set.ofSeq stepsWithDeps)
         steps
 
@@ -46,6 +58,11 @@ module Day07 =
         Set.filter (prereqsDone stepsDone) stepsToDo
         |> Set.toList
         |> List.min
+
+    let getNextReady2 (stepsToDo : Set<Step>) (stepsDone : Set<Step>) =
+        Set.filter (prereqsDone stepsDone) stepsToDo
+        |> Set.toList
+        |> List.sortBy (fun s -> s.Name)
 
     let rec findSequence sequence stepsToDo stepsDone =
         if (not (Set.isEmpty stepsToDo))
@@ -65,3 +82,65 @@ module Day07 =
         findSequence [] steps Set.empty
         |> List.map (fun s -> s.Name)
         |> List.fold (fun s c -> s + string(c)) ""
+
+    let freeWorker (workerToFree : WorkerState) (workerStates : WorkerState list) =
+        let otherWorkers = List.filter (fun w -> w.Id <> workerToFree.Id) workerStates
+        let freedWorker = { workerToFree with WorksOn = '.' }
+        otherWorkers @ [freedWorker]
+
+    let workOnStep stepToWorkOn =
+        { stepToWorkOn with TimeRequired = stepToWorkOn.TimeRequired - 1 }
+
+    let doWork (unfinishedSteps: Step list) (workerStates : WorkerState list) =
+        let stepsBeingWorkedOn = List.map (fun w -> w.WorksOn) workerStates
+        let mutable unfinishedSteps' = List.filter (fun s -> not (List.contains s.Name stepsBeingWorkedOn)) unfinishedSteps
+        let mutable workerStates' = workerStates
+        let mutable finishedSteps = []
+        for worker in workerStates do
+            if worker.WorksOn <> '.'
+            then
+                let stepToWorkOn = List.find (fun s -> s.Name = worker.WorksOn) unfinishedSteps
+                let stepWorkedOn = workOnStep stepToWorkOn
+                if (stepWorkedOn.TimeRequired = 0)
+                then
+                    workerStates' <- freeWorker worker workerStates'
+                    finishedSteps <- finishedSteps @ [stepWorkedOn]
+                else
+                    unfinishedSteps' <- unfinishedSteps' @ [stepWorkedOn]
+        (workerStates', finishedSteps, unfinishedSteps')
+
+    let scheduleFreeWorkers unfinishedSteps stepsDone (workerStates : WorkerState list) =
+        let mutable freeWorkers = List.filter (fun w -> w.WorksOn = '.') workerStates
+        let mutable workerStates' = workerStates
+        let nextReady = getNextReady2 unfinishedSteps (Set.ofList stepsDone)
+        for n in nextReady do
+            if not (List.isEmpty freeWorkers)
+            then
+                let workerAlreadyWorkingOnStep = List.exists (fun w -> w.WorksOn = n.Name) workerStates'
+                if not workerAlreadyWorkingOnStep
+                then
+                    let workerToSchedule = { freeWorkers.[0] with WorksOn = n.Name }
+                    freeWorkers <- List.filter (fun w -> w.Id <> workerToSchedule.Id) freeWorkers
+                    workerStates' <- List.filter (fun w -> w.Id <> workerToSchedule.Id) workerStates'
+                    workerStates' <- workerStates' @ [workerToSchedule]
+        workerStates'
+
+    let rec findSequence2 (sequenceOfFinishedSteps : Step list) unfinishedSteps workerStates secondsPassed =
+        if List.length sequenceOfFinishedSteps = 26
+        then
+            (secondsPassed, sequenceOfFinishedSteps)
+        else
+            let workerStates' = scheduleFreeWorkers unfinishedSteps sequenceOfFinishedSteps workerStates
+            let (workerStates'', finishedSteps, unfinishedSteps') = doWork (Set.toList unfinishedSteps) workerStates'
+            let sequenceOfSteps' = List.append sequenceOfFinishedSteps finishedSteps
+            let secondsPassed' = secondsPassed + 1
+            findSequence2 sequenceOfSteps' (Set.ofList unfinishedSteps') workerStates'' secondsPassed'
+
+    let day07Part2 () =
+        let steps = System.IO.File.ReadAllLines InputFile
+                    |> Array.map parseLine
+                    |> getSteps
+        let initialWorkerStates = List.init 5 (fun i -> { Id = i + 1; WorksOn = '.' })
+        let (seconds, _) = findSequence2 [] steps initialWorkerStates 0
+        seconds
+        
