0d838cd52905dbf6daf2663b80c8b5315c0e6419
dawe <dawedawe@gmx.de>
2019-10-05 11:30:16 +0200

    Day 09 Part 2

--- adventofcode/Day09.fs
+++ adventofcode/Day09.fs
@@ -2,24 +2,25 @@ namespace Adventofcode2018
 
 module Day09 =
 
+    open System
     open System.Collections.Generic
     open System.Linq
 
     let playerCount = 411
 
-    let lastMarbleValue = 71058
+    let lastMarbleValue = 71058L * 100L
 
     type Player =
         {
             Id : int
-            Scores : ResizeArray<int>
+            mutable Score : int64 // ResizeArray<int>
         }
 
     type GameState =
         {
             CurrentMarbleIndex : int
-            LastInsertedMarble : int
-            Marbles : ResizeArray<int>
+            LastInsertedMarble : int64
+            Marbles : ResizeArray<int64>
             Players : Player []
         }
 
@@ -27,11 +28,11 @@ module Day09 =
         let gameState =
             {
                 CurrentMarbleIndex = 0;
-                LastInsertedMarble = 0;
+                LastInsertedMarble = 0L;
                 Marbles = ResizeArray();
-                Players = Array.init playerCount (fun i -> { Id = i + 1; Scores = List<int>() })
+                Players = Array.init playerCount (fun i -> { Id = i + 1; Score = 0L })
             }
-        gameState.Marbles.Add(0)
+        gameState.Marbles.Add(0L)
         gameState
 
     let insertMarbleRight gameState nextMarbleToInsert =
@@ -44,21 +45,21 @@ module Day09 =
                          LastInsertedMarble = nextMarbleToInsert }
 
     let scoreCase gameState nextMarbleToInsert playerIndex =
-        gameState.Players.[playerIndex].Scores.Add(nextMarbleToInsert)
+        gameState.Players.[playerIndex].Score <- gameState.Players.[playerIndex].Score + nextMarbleToInsert
         let toRemoveIndex = (gameState.CurrentMarbleIndex - 7) % gameState.Marbles.Count
         let toRemoveIndexPos =
             if (toRemoveIndex < 0)
             then toRemoveIndex + gameState.Marbles.Count
             else toRemoveIndex
-        gameState.Players.[playerIndex].Scores.Add(gameState.Marbles.[toRemoveIndexPos])
+        gameState.Players.[playerIndex].Score <- gameState.Players.[playerIndex].Score + gameState.Marbles.[toRemoveIndexPos]
         gameState.Marbles.RemoveAt(toRemoveIndexPos)
         let currentMarbleIndex' = toRemoveIndexPos % gameState.Marbles.Count
         { gameState with CurrentMarbleIndex = currentMarbleIndex'
                          LastInsertedMarble = nextMarbleToInsert }
 
     let insertMarble gameState playerIndex =
-        let nextMarbleToInsert = gameState.LastInsertedMarble + 1
-        if (nextMarbleToInsert % 23) <> 0
+        let nextMarbleToInsert = gameState.LastInsertedMarble + 1L
+        if (nextMarbleToInsert % 23L) <> 0L
         then
             insertMarbleRight gameState nextMarbleToInsert
         else
@@ -75,5 +76,5 @@ module Day09 =
     let day09 () =
         let startState = initGame()
         let gameState = play startState 0
-        let winner = Array.maxBy (fun p -> p.Scores.Sum()) gameState.Players
-        winner.Scores.Sum()
+        let winner = Array.maxBy (fun p -> p.Score) gameState.Players
+        winner.Score
