319dd8de6b3d1ad3a7a3908b085299acef56d9ac
dawe <dawedawe@gmx.de>
2020-06-21 23:41:56 +0200

    Day 19 Part 2

--- adventofcode/Day19.fs
+++ adventofcode/Day19.fs
@@ -4,21 +4,21 @@ module Day19 =
 
     open System
 
-    type RegisterState = int []
+    type RegisterState = int64 []
 
     type ProgramState =
         { RegState: RegisterState
-          InstPointerReg: int
-          InstPointer: int }
+          InstPointerReg: int64
+          InstPointer: int64 }
 
     type Input =
-        | Register of int
-        | Value of int
+        | Register of int64
+        | Value of int64
 
     type InstructionParams =
-        { InputA: int
-          InputB: int
-          OutReg: int }
+        { InputA: int64
+          InputB: int64
+          OutReg: int64 }
 
     type Instruction =
         { Opcode: InstructionParams -> ProgramState -> ProgramState
@@ -33,156 +33,156 @@ module Day19 =
 
     let addr (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- regsAfter.[inst.InputA] + regsAfter.[inst.InputB]
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- regsAfter.[int inst.InputA] + regsAfter.[int inst.InputB]
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let addi (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- regsAfter.[inst.InputA] + inst.InputB
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- regsAfter.[int inst.InputA] + inst.InputB
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let mulr (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- regsAfter.[inst.InputA] * regsAfter.[inst.InputB]
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- regsAfter.[int inst.InputA] * regsAfter.[int inst.InputB]
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let muli (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- regsAfter.[inst.InputA] * inst.InputB
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- regsAfter.[int inst.InputA] * inst.InputB
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let banr (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- regsAfter.[inst.InputA] &&& regsAfter.[inst.InputB]
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- regsAfter.[int inst.InputA] &&& regsAfter.[int inst.InputB]
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let bani (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- regsAfter.[inst.InputA] &&& inst.InputB
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- regsAfter.[int inst.InputA] &&& inst.InputB
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let borr (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- regsAfter.[inst.InputA] ||| regsAfter.[inst.InputB]
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- regsAfter.[int inst.InputA] ||| regsAfter.[int inst.InputB]
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let bori (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- regsAfter.[inst.InputA] ||| inst.InputB
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- regsAfter.[int inst.InputA] ||| inst.InputB
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let setr (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- regsAfter.[inst.InputA]
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- regsAfter.[int inst.InputA]
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let seti (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- inst.InputA
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- inst.InputA
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let gtir (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- if (inst.InputA > regsAfter.[inst.InputB])
-                                   then 1
-                                   else 0
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- if (inst.InputA > regsAfter.[int inst.InputB])
+                                       then 1L
+                                       else 0L
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let gtri (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- if (regsAfter.[inst.InputA] > inst.InputB)
-                                   then 1
-                                   else 0
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- if (regsAfter.[int inst.InputA] > inst.InputB)
+                                       then 1L
+                                       else 0L
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let gtrr (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- if (regsAfter.[inst.InputA] > regsAfter.[inst.InputB])
-                                   then 1
-                                   else 0
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- if (regsAfter.[int inst.InputA] > regsAfter.[int inst.InputB])
+                                       then 1L
+                                       else 0L
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let eqir (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- if (inst.InputA = regsAfter.[inst.InputB])
-                                   then 1
-                                   else 0
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- if (inst.InputA = regsAfter.[int inst.InputB])
+                                       then 1L
+                                       else 0L
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let eqri (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- if (regsAfter.[inst.InputA] = inst.InputB)
-                                   then 1
-                                   else 0
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- if (regsAfter.[int inst.InputA] = inst.InputB)
+                                       then 1L
+                                       else 0L
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
 
     let eqrr (inst: InstructionParams) (state: ProgramState) =
         let regsAfter = Array.copy state.RegState
-        regsAfter.[state.InstPointerReg] <- state.InstPointer
-        regsAfter.[inst.OutReg] <- if (regsAfter.[inst.InputA] = regsAfter.[inst.InputB])
-                                   then 1
-                                   else 0
-        let instPointer = regsAfter.[state.InstPointerReg] + 1
+        regsAfter.[int state.InstPointerReg] <- state.InstPointer
+        regsAfter.[int inst.OutReg] <- if (regsAfter.[int inst.InputA] = regsAfter.[int inst.InputB])
+                                       then 1L
+                                       else 0L
+        let instPointer = regsAfter.[int state.InstPointerReg] + 1L
         { state with
               RegState = regsAfter
               InstPointer = instPointer }
@@ -212,15 +212,15 @@ module Day19 =
         let opcode = opcodesMap.[opcodeName]
 
         let p =
-            { InputA = Int32.Parse(parts.[1])
-              InputB = Int32.Parse(parts.[2])
-              OutReg = Int32.Parse(parts.[3]) }
+            { InputA = Int64.Parse(parts.[1])
+              InputB = Int64.Parse(parts.[2])
+              OutReg = Int64.Parse(parts.[3]) }
 
         { Opcode = opcode; Params = p }
 
     let parseInstPointerLine (s: string) =
         let parts = s.Split [| ' ' |]
-        Int32.Parse(parts.[1])
+        Int64.Parse(parts.[1])
 
     let getProgram path =
         let lines = System.IO.File.ReadAllLines path
@@ -230,19 +230,19 @@ module Day19 =
             lines.[1..] |> Array.map parseInstruction
 
         let progState =
-            { RegState = Array.create 6 0
+            { RegState = Array.create 6 0L
               InstPointerReg = ipReg
-              InstPointer = 0 }
+              InstPointer = 0L }
 
         { State = progState
           Instructions = instructions }
 
     let rec runProgram program =
-        if (program.State.InstPointer < 0 || program.State.InstPointer >= program.Instructions.Length)
+        if (program.State.InstPointer < 0L || program.State.InstPointer >= int64 program.Instructions.Length)
         then program
         else
-            let op = program.Instructions.[program.State.InstPointer]
-            let programState' = op.Opcode program.Instructions.[program.State.InstPointer].Params program.State
+            let op = program.Instructions.[int program.State.InstPointer]
+            let programState' = op.Opcode program.Instructions.[int program.State.InstPointer].Params program.State
             let program' = { program with State = programState' }
             runProgram program'
 
@@ -250,3 +250,44 @@ module Day19 =
         let program = getProgram InputFile
                       |> runProgram
         program.State.RegState.[0]
+    
+    let shortcut (program : Program) =
+        program.State.RegState.[5] <- 1L
+        program.State.RegState.[3] <- 1L
+        while int program.State.RegState.[2] < program.Instructions.Length do
+
+            program.State.RegState.[3] <- program.State.RegState.[1] / program.State.RegState.[5]
+            program.State.RegState.[4] <- program.State.RegState.[5] * program.State.RegState.[3]
+            
+            if program.State.RegState.[4] = program.State.RegState.[1]
+            then program.State.RegState.[0] <- program.State.RegState.[5] + program.State.RegState.[0]
+
+            program.State.RegState.[3] <- program.State.RegState.[1] + 1L
+
+            if program.State.RegState.[3] > program.State.RegState.[1]
+            then
+                program.State.RegState.[5] <- program.State.RegState.[5] + 1L
+                if program.State.RegState.[5] > program.State.RegState.[1]
+                then program.State.RegState.[2] <- int64 program.Instructions.Length
+                else program.State.RegState.[3] <- 1L
+
+        { program with State = { program.State with InstPointer = program.State.RegState.[int program.State.InstPointerReg]} }
+
+    let rec runProgramPart2 program =
+        if (program.State.InstPointer < 0L || program.State.InstPointer >= int64 program.Instructions.Length)
+        then program
+        else
+            if (program.State.InstPointer = 1L)
+            then shortcut program
+            else
+                let op = program.Instructions.[int program.State.InstPointer]
+                let programState' = op.Opcode program.Instructions.[int program.State.InstPointer].Params program.State
+                let program' = { program with State = programState' }
+                runProgramPart2 program'
+
+    let day19Part2 () =
+        let program = getProgram InputFile
+        program.State.RegState.[0] <- 1L
+        let program' = runProgramPart2 program
+        program'.State.RegState.[0]
+        
--- adventofcode/Program.fs
+++ adventofcode/Program.fs
@@ -4,6 +4,6 @@ open Adventofcode2018
 
 [<EntryPoint>]
 let main argv =
-    let r = Day19.day19()
+    let r = Day19.day19Part2()
     printfn "%A" r
     0 // return an integer exit code
